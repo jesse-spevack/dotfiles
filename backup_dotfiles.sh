022131a92482112b79079617fdad54da4738accb
Jesse Spevack <jesse.spevack@ibotta.com>
2021-02-28 21:52:59 -0700

    Update: 28-02-2021 @ 21:52:59

---
+++ backup_dotfiles.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# create a timestamp alias for the commit message
+timestamp() {
+  date +"%d-%m-%Y @ %T"
+}
+
+# files to backup
+cp /Users/jesse.spevack/.zshrc /Users/jesse.spevack/codes/dotfiles/zshrc
+cp /Users/jesse.spevack/.asdfrc /Users/jesse.spevack/codes/dotfiles/asdfrc
+cp /Users/jesse.spevack/.gitconfig /Users/jesse.spevack/codes/dotfiles/gitconfig
+
+cd /Users/jesse.spevack/codes/dotfiles
+
+# pull & push
+if [[ `git status --porcelain` ]]; then
+    git pull origin main
+    git add .
+    git commit -m "Update: $(timestamp)"
+    git push origin main
+fi
