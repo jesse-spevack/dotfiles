68c75e275eb2a690e974cf7c0fd2089d6a086f59
Jesse Spevack <jesse.spevack@ibotta.com>
2021-03-15 08:35:35 -0600

    Update: 2021-03-15 @ 08:35:35

--- README.md
+++ README.md
@@ -5,5 +5,3 @@ Handle secret info with: [this](https://github.com/onyxraven/zsh-osx-keychain)
 Enable the .sh to run by:
 1. Open [crontab](http://crontab.org/) `crontab -e`
 2. Add `0 9 * * MON cd ~/codes/dotfiles && ./backup_dotfiles.sh` to run the backup_dotfiles script on Monday at 9 am. See [Crontab.guru](https://crontab.guru/#*_*_*)
-
-Help.
--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -12,10 +12,16 @@ cp /Users/jesse.spevack/.gitconfig /Users/jesse.spevack/codes/dotfiles/gitconfig
 
 cd /Users/jesse.spevack/codes/dotfiles
 
+echo "job started: $(timestamp)" > log.txt
+
 # pull & push
 if [[ `git status --porcelain` ]]; then
-    git pull origin main
-    git add .
-    git commit -m "Update: $(timestamp)"
-    git push origin main
+  echo "Running Git commands: $(timestamp)" >> log.txt
+  git pull origin main 2>&1 | tee -a log.txt
+  git add . 2>&1 | tee -a log.txt
+  git commit -m "Update: $(timestamp)" 2>&1 | tee -a log.txt
+  git push origin main 2>&1 | tee -a log.txt
+  echo "Finished running Git commands: $(timestamp)" >> log.txt
 fi
+
+echo "job done: $(timestamp)" >> log.txt
